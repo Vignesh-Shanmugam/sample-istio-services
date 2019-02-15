6e1431b0363c862deb2959beec92f794f3e79d91
Vignesh-Shanmugam <s.vig14@gmail.com>
2019-02-15 10:36:44 -0500

    Add files via upload

---
+++ caller-service-jwt/src/main/java/com/bns/services/caller/controller/CallerController.java
@@ -0,0 +1,37 @@
+package com.bns.services.caller.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/jwtcaller")
+public class CallerController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(CallerController.class);
+	
+	@Autowired
+	BuildProperties buildProperties;
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@GetMapping("/ping")
+	public String ping() {
+		LOGGER.info("Ping: name={}, version={}", buildProperties.getName(), buildProperties.getVersion());
+		String response = restTemplate.getForObject("http://caller-service:8090/caller/ping", String.class);
+		LOGGER.info("Calling: response={}", response);
+		return buildProperties.getName() + ":" + buildProperties.getVersion() + ". Calling... " + response; 
+	}
+
+	@GetMapping("/check")
+	public String check() {
+		LOGGER.info("Checking....");
+		return "caller jwt service working"; 
+	}
+	
+}
